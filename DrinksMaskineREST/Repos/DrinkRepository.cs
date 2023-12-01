0bf1c8d37444a4e7b20319893e95173c98d7827d
Andreas Dragsberg <115148828+ADragsberg@users.noreply.github.com>
2023-12-01 10:58:39 +0100

    fixed name of Drink and added repo

---
+++ DrinksMaskineREST/Repos/DrinkRepository.cs
@@ -0,0 +1,21 @@
+﻿using DrinksMaskineREST.Models;
+using System.Runtime.CompilerServices;
+
+namespace DrinksMaskineREST.Repos
+{
+    public class DrinkRepository : IDrinkRepository
+    {
+        private Dictionary<int, Drink> _drinks;
+        public DrinkRepository() 
+        {
+
+            // hardcoded data
+            _drinks = new Dictionary<int, Drink>();
+        }
+
+        public Drink Add()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- DrinksMaskineREST/Repos/IDrinkRepository.cs
+++ DrinksMaskineREST/Repos/IDrinkRepository.cs
@@ -1,6 +1,9 @@
-﻿namespace DrinksMaskineREST.Repos
+﻿using DrinksMaskineREST.Models;
+
+namespace DrinksMaskineREST.Repos
 {
     public interface IDrinkRepository
     {
+        Drink Add();
     }
 }
