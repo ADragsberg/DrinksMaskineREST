fda27f1b1dc54bbea12afc1196f1ae0bab513167
Andreas Dragsberg <115148828+ADragsberg@users.noreply.github.com>
2023-12-06 11:34:49 +0100

    test objects for repo test

--- DrinksMaskineRESTTests/Repos/DrinkDBRepositoryTests.cs
+++ DrinksMaskineRESTTests/Repos/DrinkDBRepositoryTests.cs
@@ -8,6 +8,7 @@ using System.Threading.Tasks;
 using Microsoft.Extensions.Options;
 using Secret;
 using Microsoft.EntityFrameworkCore;
+using DrinksMaskineREST.Models;
 
 namespace DrinksMaskineREST.Repos.Tests
 {
@@ -15,6 +16,31 @@ namespace DrinksMaskineREST.Repos.Tests
     public class DrinkDBRepositoryTests
     {
         private static IDrinkRepository _repository;
+        private DrinkModel _validMinDrink = new DrinkModel() {
+            idDrink = null,
+            strDrink = "V",
+            strCategory = "V",
+            strAlcoholic = "alcoholic",
+            strIngredient1 = "V",
+            strIngredient2 = "V",
+            strMeasure1 = "1 oz",
+            strMeasure2 = "1 oz",
+            Id = 1
+        };
+                
+        private DrinkModel _validMaxDrink = new DrinkModel() { 
+            idDrink = null , 
+            strDrink = "qqqqq wwwww eeeee rrrrr ttttt yyyyy uuuuu iiiii ppppp aaaaa ssss", //64 characters with spaces
+            strCategory = "qqqqq wwwww eeeee rrrrr ttttt yyyyy uuuuu iiiii ppppp aaaaa ssss",
+            strAlcoholic = "NON ALCOHOLIC",
+            strIngredient1 = "qqqqq wwwww eeeee rrrrr ttttt yyyyy uuuuu iiiii ppppp aaaaa ssss",
+            strIngredient2 = "qqqqq wwwww eeeee rrrrr ttttt yyyyy uuuuu iiiii ppppp aaaaa ssss",
+            strMeasure1 = "1 L",
+            strMeasure2 = "1 L",
+            Id = 1
+        };
+
+        private DrinkModel _invalid = new DrinkModel(){strDrink = ""};
 
         [ClassInitialize]
         public static void RepoSetup()
@@ -36,7 +62,7 @@ namespace DrinksMaskineREST.Repos.Tests
         [TestMethod()]
         public void AddTest()
         {
-            Assert.Fail();
+            
         }
         
         [TestMethod()]
