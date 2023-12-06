e14a76b407dc32caee603cda9ee651203957e7b7
Andreas Dragsberg <115148828+ADragsberg@users.noreply.github.com>
2023-12-06 13:44:56 +0100

    AddTest Passed

--- DrinksMaskineRESTTests/Repos/DrinkDBRepositoryTests.cs
+++ DrinksMaskineRESTTests/Repos/DrinkDBRepositoryTests.cs
@@ -19,13 +19,14 @@ namespace DrinksMaskineREST.Repos.Tests
         private DrinkModel _validMinDrink = new DrinkModel() {
             idDrink = null,
             strDrink = "V",
-            strCategory = "V",
+            strCategory = "V1",
             strAlcoholic = "alcoholic",
-            strIngredient1 = "V",
-            strIngredient2 = "V",
+            strIngredient1 = "V1",
+            strIngredient2 = "B1",
             strMeasure1 = "1 oz",
             strMeasure2 = "1 oz",
-            Id = 1
+            Id = 0,
+            Creator = "V"
         };
                 
         private DrinkModel _validMaxDrink = new DrinkModel() { 
@@ -34,16 +35,29 @@ namespace DrinksMaskineREST.Repos.Tests
             strCategory = "qqqqq wwwww eeeee rrrrr ttttt yyyyy uuuuu iiiii ppppp aaaaa ssss",
             strAlcoholic = "NON ALCOHOLIC",
             strIngredient1 = "qqqqq wwwww eeeee rrrrr ttttt yyyyy uuuuu iiiii ppppp aaaaa ssss",
-            strIngredient2 = "qqqqq wwwww eeeee rrrrr ttttt yyyyy uuuuu iiiii ppppp aaaaa ssss",
-            strMeasure1 = "1 L",
-            strMeasure2 = "1 L",
-            Id = 1
+            strIngredient2 = "qqqqq wwwww eeeee rrrrr ttttt yyyyy uuuuu iiiii ppppp aaaaa dddd",
+            strMeasure1 = "100 L",
+            strMeasure2 = "100 L",
+            Id = 0,
+            Creator = "qqqqq wwwww eeeee rrrrr ttttt yyyyy uuuuu iiiii ppppp aaaaa ssss"
         };
 
         private DrinkModel _invalid = new DrinkModel(){strDrink = ""};
 
+        private DrinkModel _invalidForFaaIngr = new DrinkModel()
+        {
+            idDrink = null,
+            strDrink = "V",
+            strCategory = "V",
+            strAlcoholic = "alcoholic",
+            strIngredient1 = "V",
+            strMeasure1 = "1 oz",
+            Id = 1,
+            Creator = "V"
+        };
+
         [ClassInitialize]
-        public static void RepoSetup()
+        public static void InitOnce(TestContext testContext)
         {
             var optionsBuilder = new DbContextOptionsBuilder<DrinksDBContext>();
             optionsBuilder.UseSqlServer(Secrets.ConnectionString);
@@ -62,6 +76,27 @@ namespace DrinksMaskineREST.Repos.Tests
         [TestMethod()]
         public void AddTest()
         {
+            DrinkModel drink1 = _repository.Add(_validMinDrink);
+            DrinkModel drink2 = _repository.Add(_validMaxDrink);
+
+            //DrinkNavn
+            Assert.IsTrue(_repository.GetById(drink1.Id).strDrink == _validMinDrink.strDrink);
+            Assert.IsTrue(_repository.GetById(drink2.Id).strDrink == _validMaxDrink.strDrink);
+
+            //CreatorNavn
+            Assert.IsTrue(_repository.GetById(drink1.Id).Creator == _validMinDrink.Creator);
+            Assert.IsTrue(_repository.GetById(drink2.Id).Creator == _validMaxDrink.Creator);
+
+            //Ingredienser
+            Assert.IsTrue(_repository.GetById(drink1.Id).strIngredient1 == _validMinDrink.strIngredient1);
+            Assert.IsTrue(_repository.GetById(drink1.Id).strIngredient2 == _validMinDrink.strIngredient2);
+
+            Assert.IsTrue(_repository.GetById(drink2.Id).strIngredient1 == _validMaxDrink.strIngredient1);
+            Assert.IsTrue(_repository.GetById(drink2.Id).strIngredient2 == _validMaxDrink.strIngredient2);
+
+            Assert.ThrowsException<Exception>(() => _repository.Add(_invalidForFaaIngr));
+
+            Assert.ThrowsException<Exception>(() => _repository.Add(_invalid));
             
         }
         
